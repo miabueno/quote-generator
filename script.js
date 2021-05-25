6e4819155f89ed6d7d0ccbc9f0199f89bb33a6a6
Mia Bueno <mia.bueno101@gmail.com>
2021-05-25 16:10:37 +1000

    init commit - display new quote on refresh

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let apiQuotes = [];
+
+// Show new quote in quote container
+function displayNewQuote(quotesArr) {
+    const newQuote = randomQuote(quotesArr);
+    const quoteText = newQuote.text;
+    const quoteAuthor = newQuote.author ? newQuote.author : 'Unknown';
+
+    // DOM elements
+    const quoteTextEl = document.getElementById('quote');
+    const quoteAuthorEl = document.getElementById('author');
+
+    // quote length to determine styling
+    (quoteText.length > 120) ? quoteTextEl.classList.add('long-quote') 
+        : quoteTextEl.classList.remove('long-quote');
+
+    // manipulate DOM elements
+    quoteTextEl.textContent = quoteText;
+    quoteAuthorEl.textContent = quoteAuthor;
+}
+
+// Select a random quote from given array
+function randomQuote(quotesArr) {
+    const randomIdx = Math.floor(Math.random() * quotesArr.length);
+    return quotesArr[randomIdx];
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        
+        displayNewQuote(apiQuotes);
+
+    } catch(error) {
+        alert(error);
+    }
+}
+
+// On Load
+getQuotes();
